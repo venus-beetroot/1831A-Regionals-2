23614caaec3168b9ad2fc4761f74f7e011e07d17
Haoran Fang <ifh_fang.h@outlook.com>
2024-09-07 19:34:08 +1000

    Add files via upload
    Regionals 2 Code Ver 1.0

---
+++ robot-config.h
@@ -0,0 +1,32 @@
+#include "vex.h"
+
+
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern drivetrain Drivetrain;
+extern motor_group Intake;
+extern digital_out Clamp;
+extern controller Controller1;
+extern motor leftMotorA;
+extern motor leftMotorB;
+extern motor leftMotorC;
+extern motor_group RightDriveSmart;
+extern motor rightMotorA;
+extern motor rightMotorB;
+extern motor rightMotorC;
+extern motor_group LeftDriveSmart;
+extern motor IntakeMotorA;
+extern motor IntakeMotorB;
+extern motor_group Intake;
+extern inertial Inertial;
+
+
+/**
+ 
+Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+This should be called at the start of your int main function.
+*/
+void  vexcodeInit( void );
